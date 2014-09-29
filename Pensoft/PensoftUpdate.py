99c54a833c4b76633e8b7b81b994d080acfb76d3
Jason Karl <jakal14@gmail.com>
2014-09-29 13:22:40 -0600

    First working version of Pensoft Updater
    Finished first version of Pensoft Updater. Grabs XML files since last
    update and saves them to specified output directory.

--- Pensoft/PensoftUpdate.py
+++ Pensoft/PensoftUpdate.py
@@ -1,17 +1,38 @@
 ### PensoftUpdate.py
-### Script for downloading and saving Pensoft content from a given date to present.
+### Script for downloading and saving Pensoft content XML files from their API for a given date to present.
 
 
-import requests, StringIO, time
+import requests, time, json
 
 
+## Set up constants
 journallist = ['zookeys','phytokeys','biorisk','neobiota','jhr','ijm','compcytogen','subtbiol','natureconservation','mycokeys','zse','dez','nl','ab']
-urldate = time.strftime("%d-%m-%Y").replace('-','%2F')
 urlbase = 'http://bdj.pensoft.net/lib/journal_archive.php?journal='
+outpath = '/Users/jason/Downloads/'
+paramsfile = 'PensoftHistory.json'
 
+## Fetch the last date the script was run
+with open(paramsfile,'rb') as pFile:
+    param_json = json.load(pFile)
+numUpdates = len(param_json['updates'])
+lastDate=param_json['updates'][numUpdates-1]['date']
+
+urldate = lastDate.replace('-','%2F')
+
+## Grab any new content from Pensoft
 for journal in journallist:
     r = requests.get(urlbase+journal+'&date='+urldate)
-    output = open('/Users/jason/Downloads/test.zip', 'w')
+    output = open(outpath+journal+'-'+lastDate+'.zip', 'w')
     output.write(r.content)
     output.close()
 
+
+## Update the params file with the new date
+day = time.strftime('%d').strip('0')
+month = time.strftime('%m').strip('0')
+year = time.strftime('%Y')
+newdate = day+'-'+month+'-'+year
+update = {'date':newdate}
+param_json['updates'].append(update)    
+with open(paramsfile,'wb') as pFile:
+    json.dump(param_json, pFile)
